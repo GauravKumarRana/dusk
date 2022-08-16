111520612243c1fb7137ccd36b01a251079becb7
Gaurav Kumar Rana <gkrana97@gmail.com>
2022-08-16 12:38:57 +0530

    new commit

--- dusk/shop/views.py
+++ dusk/shop/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse('This is homepage.')
+    context = { 'variable' : 'this is variable message'} # this is a python dictionary
+    return render(request, 'index.html', context)
+    # return HttpResponse('This is homepage.')
 
 def about(request):
     return HttpResponse('This is about page.')
+
+def services(request):
+    return HttpResponse('This is services page.')
+
+def contact(request):
+    return HttpResponse('This is contact page.')
